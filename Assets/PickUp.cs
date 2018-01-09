2531a7a4a4e70e8de120f3554f0fdd3edeaae3ab
Cameron Zach P Tucker <s179071@students.aie.edu.au>
2018-01-09 08:57:05 -0800

    first update

---
+++ Assets/PickUp.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            
+            BallController ballController = other.GetComponent<BallController>();
+            ballController.score += 1;
+            Destroy(gameObject);
+        }
+    }
+
+    //OnTriggerEnter(Collider other)
+
+    //if the colliders tag is the player
+    //Get the BallController componenet and add score
+
+}
---
+++ Assets/RespawnController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Vector3 spawn = new Vector3(0, 9, 1);
+            other.transform.position = spawn;
+            BallController ballController = other.GetComponent<BallController>();
+            ballController.rb.velocity = Vector3.zero;
+        }
+    }
+}
